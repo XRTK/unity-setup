c59a634693e100ff1fc5b77b90f9afb8def36d95
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-06 14:48:47 -0500

    didn't set failure correctly

--- dist/index.js
+++ dist/index.js
@@ -4124,11 +4124,7 @@ const main = async () => {
 
         console.log(`::group::Run xrtk/unity-setup`);
 
-        try{
-            exitCode = await exec.exec(`"${pwsh}" -Command`, `${install} ${args}`);
-        } catch {
-            // Nothing
-        }
+        exitCode = await exec.exec(`"${pwsh}" -Command`, `${install} ${args}`);
 
         console.log(`::endgroup::`);
 
