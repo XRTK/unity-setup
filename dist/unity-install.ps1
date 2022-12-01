bba5950771f6e8850e9b0d8e443b084a8bdf8a0a
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-01 15:17:10 -0500

    update key

--- dist/unity-install.ps1
+++ dist/unity-install.ps1
@@ -118,7 +118,7 @@ if ( -not (Test-Path -Path "$hubPath") ) {
     elseif ($global:PSVersionTable.OS.Contains("Linux")) {
         #sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.10_amd64.deb
         sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-        wget -qO - https://hub.unity3d.com/linux/keys/public | sudo tee /etc/apt/trusted.gpg.d/unityhub.asc
+        wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
         sudo apt-get update
         sudo apt-get install unityhub
     }
