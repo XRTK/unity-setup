1ccb51ecd0fca51ae51a166a41eaf270c25224c4
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-01 12:49:35 -0500

    try a different method to install openssl1.1.1

--- src/unity-install.ps1
+++ src/unity-install.ps1
@@ -117,19 +117,13 @@ if ( -not (Test-Path -Path "$hubPath") ) {
     }
     elseif ($global:PSVersionTable.OS.Contains("Linux")) {
         #Lib issue Ubuntu
-        wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
-        chmod +x libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
-        sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
-        echo "deb http://security.ubuntu.com/ubuntu impish-security main" | sudo tee /etc/apt/sources.list.d/impish-security.list
-        sudo apt-get update
-        sudo apt-get install libssl1.1
+        sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.10_amd64.deb
 
         #unityhub
         sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-        wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
+        wget -qO - https://hub.unity3d.com/linux/keys/public | sudo tee /etc/apt/trusted.gpg.d/unityhub.asc
         sudo apt update
         sudo apt-get install unityhub
-        sudo apt install libfuse2
     }
 
     Write-Host "::endgroup::"
