e0db40df6e7a2cae7d934b0adeb87d638c89ee64
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-19 13:32:13 -0500

    change default installation hub path

--- dist/unity-install.ps1
+++ dist/unity-install.ps1
@@ -58,7 +58,7 @@ elseif ( $global:PSVersionTable.OS.Contains("Darwin") ) {
     }
 }
 elseif ( $global:PSVersionTable.OS.Contains("Linux") ) {
-    $hubPath = "$HOME/Applications/Unity Hub.AppImage"
+    $hubPath = "~/Applications/Unity Hub.AppImage"
     $editorRootPath = "$HOME/Unity/Hub/Editor/"
     $editorFileEx = "/Editor/Unity"
 
