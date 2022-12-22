0d713c1de33d9ade861d42e222900b0953c8cda4
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-21 23:28:01 -0500

    made some changes

--- src/unity-install.ps1
+++ src/unity-install.ps1
@@ -118,8 +118,8 @@ if ( -not (Test-Path -Path "$hubPath") ) {
     elseif ($global:PSVersionTable.OS.Contains("Linux")) {
         sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
         wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
-        sudo apt-get update
-        sudo apt-get install unityhub
+        sudo apt update
+        sudo apt install unityhub
         which unityhub
     }
 
