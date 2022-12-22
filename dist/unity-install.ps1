01956040abb90eae0947b1ff7dbb172b2f0fddec
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-21 23:31:02 -0500

    try again

--- dist/unity-install.ps1
+++ dist/unity-install.ps1
@@ -119,7 +119,7 @@ if ( -not (Test-Path -Path "$hubPath") ) {
         sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
         wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
         sudo apt update
-        sudo apt install unityhub
+        sudo apt-get install unityhub
         which unityhub
     }
 
